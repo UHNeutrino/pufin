f38800203369052b58255cdd4eebc4211ec1faac
Lars Bøe <larsw00@outlook.com>
2025-01-16 12:08:30 -0600

    Neut Plotting script

--- neutPlots.py
+++ neutPlots.py
@@ -18,10 +18,12 @@ histInfo2 = ("name",f"{v1} vs {v2} plot",60,0,3,60,0,3)
 ParticlePlots.Plot1PI(v1,v2,histInfo1,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6.root")
 ParticlePlots.Plot1PI(v1,v2,histInfo1,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_1.0GeV_1e6.root")
 ParticlePlots.Plot1PI(v1,v2,histInfo1,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_1.5GeV_1e6.root")
-ParticlePlots.Plot1PI(v1,v2,histInfo1,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_3.0GeV_1e6.root")
+ParticlePlots.Plot1PI(v1,v2,histInfo1,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_3GeV_1e6.root")
 
+v1 = 'q0'
+v2 = 'q3'
 
 ParticlePlots.Plot2P2H(v1,v2,histInfo2,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6.root")
 ParticlePlots.Plot2P2H(v1,v2,histInfo2,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_1.0GeV_1e6.root")
 ParticlePlots.Plot2P2H(v1,v2,histInfo2,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_1.5GeV_1e6.root")
-ParticlePlots.Plot2P2H(v1,v2,histInfo2,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_3.0GeV_1e6.root")
+ParticlePlots.Plot2P2H(v1,v2,histInfo2,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_3GeV_1e6.root")
