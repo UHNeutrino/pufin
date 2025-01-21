7930214955164f3f5c9c0e7d964d939ddfdf7378
Lars Bøe <larsw00@outlook.com>
2025-01-21 17:36:46 -0600

    Added Normalizing and a macro to spin over all your files!

--- ParticlePlots.py
+++ ParticlePlots.py
@@ -13,7 +13,7 @@ ROOT.gStyle.SetNumberContours(50)     # Increase the number of colors in the gra
 # lets me use other people's home directories
 HOME = os.getenv("HOME", "/home/lboe")
 
-# enables multiprocessing **currently has not multiprocessing***
+# enables multiprocessing **currently has no multiprocessing***
 # ROOT.EnableImplicitMT()
 
 # Allows python to manage the memeory rather than ROOT ***might be causing seg faults***
@@ -61,19 +61,12 @@ def formatTcanvas(hist, c):
     # c.SetCanvasSize(600,500)
     c.SetCanvasSize(c.GetWw()+200,c.GetWh())
 
-def Plot2P2H(x, y, histogramInfo, title, file_path = None, max = None):
+def Plot2P2H(x, y, histogramInfo, title, file_path = None, max = None, Normalize = 0):
     # First get the data into a dataframe
     if file_path is None:
         dir_location = input("Give Flat Tree Directory Location (not including home): ")
     else:
         dir_location = file_path
-    # fileName = f"{HOME}/{dir_location}"
-    # treeName = "FlatTree_VARS"
-    # parts = fileName.split('/')
-    # NameRoot = parts[5]
-    # NameParts = NameRoot.split('_')
-    # NameParts[3] = NameParts[3].split('.root')[0]
-    # Name = NameParts[1] + "_" + NameParts[2] + "_" + NameParts[3]
     fileName, treeName, NameParts, Name = formatName(dir_location)
 
     df = ROOT.RDataFrame(treeName,fileName)
@@ -81,6 +74,12 @@ def Plot2P2H(x, y, histogramInfo, title, file_path = None, max = None):
     # Mode 2 is the 2P2H interaction
     cut1 = 'Mode == 2'
     hist1 = df.Filter(cut1).Histo2D(histogramInfo,x,y)
+
+    if Normalize == 1:
+       scale = 1/(hist1.Integral())
+    #    print(scale)
+       hist1.Scale(scale)
+
 
     if max is None:
         hist = formatHist(NameParts, hist1 ,'q_{3}', '(GeV)', 'q_{0}', '(GeV)')
@@ -92,11 +91,13 @@ def Plot2P2H(x, y, histogramInfo, title, file_path = None, max = None):
     # saves hist to a specific directory 
     if max is not None:
         c.SaveAs(f"{HOME}/t2k-nova/plots_constant_z_axis/{title}_{Name}.png")
+    if Normalize == 1:
+        c.SaveAs(f"{HOME}/t2k-nova/plots_normalized/{title}_{Name}.png")
     else:
         c.SaveAs(f"{HOME}/t2k-nova/plots/{title}_{Name}.png")
 
 
-def Plot1PI(x, y, histogramInfo, title, file_path = None, max = None):
+def Plot1PI(x, y, histogramInfo, title, file_path = None, max = None, Normalize = 0):
     if file_path is None:
         dir_location = input("Give Flat Tree Directory Location (not including home): ")
     else:
@@ -109,6 +110,13 @@ def Plot1PI(x, y, histogramInfo, title, file_path = None, max = None):
     # Modes for single Pi are 11-16
     cut1 = 'Mode == 11 || Mode ==  12 || Mode == 13 || Mode == 14 || Mode == 15 || Mode == 16 '
     hist1 = df.Filter(cut1).Histo2D(histogramInfo,x,y)
+
+    if Normalize == 1:
+       scale = 1/(hist1.Integral())
+    #    print(scale)
+       hist1.Scale(scale)
+
+
     if max is None:
         hist = formatHist(NameParts, hist1,'W', '(GeV)', 'Q^{2}', '(GeV)^{2}')
     else:
@@ -118,6 +126,8 @@ def Plot1PI(x, y, histogramInfo, title, file_path = None, max = None):
     # saves hist to a plots directory
     if max is not None:
         c.SaveAs(f"{HOME}/t2k-nova/plots_constant_z_axis/{title}_{Name}.png")
+    if Normalize == 1:
+        c.SaveAs(f"{HOME}/t2k-nova/plots_normalized/{title}_{Name}.png")
     else:
         c.SaveAs(f"{HOME}/t2k-nova/plots/{title}_{Name}.png")
 
@@ -126,11 +136,11 @@ if __name__=="__main__":
     x = 'q3'
     y = 'q0'
     histInfo = ("name",f"{y} vs {x} plot",60,0,3,60,0,3)
-    Plot2P2H(x,y,histInfo,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6.root")
-    x = 'W'
-    y = 'Q2'
-    histInfo = ("name",f"{y} vs {x} plot",60,0,3,120,0,6)
-    Plot1PI(x,y,histInfo,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6.root")
+    Plot2P2H(x,y,histInfo,"2P2H_hist","t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6.root", Normalize = 1)
+    # x = 'W'
+    # y = 'Q2'
+    # histInfo = ("name",f"{y} vs {x} plot",60,0,3,120,0,6)
+    # Plot1PI(x,y,histInfo,"1PI_hist","t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6.root")
 
 
 
