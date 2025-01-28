66ddf6a85325138a57399240581a1c3c0fbac4f2
Lars Bøe <larsw00@outlook.com>
2025-01-28 16:18:32 -0600

    won't run the max freq plotter

--- Plot_macro.py
+++ Plot_macro.py
@@ -46,19 +46,22 @@ print(f"The highest frequency across all normalized 1PI plots is: {max_frequency
 print(f"The highest frequency across all normalized 2P2H plots is: {max_frequency_2P2H}")
 
 # Now plot constant frequency (z axis) files using max_frequency_1PI and max_frequency_2P2H
-for file in root_files:
-    # format correctly
-    file_path = file.split('t2k-nova')[1]
-    file_path = 't2k-nova' + file_path
-    
-    x = 'W'
-    y = 'Q2'
-    histInfo1 = ("name",f"{x} vs {y} plot",60,0,3,124,-0.2,6)
-    histInfo2 = ("name",f"{x} vs {y} plot",60,0,3,60,0,3)
-    
-    pp.Plot1PI(x,y,histInfo1,"1PI_hist_max",file_path, max = max_frequency_1PI, Normalize=1)
-    
-    x = 'q3'
-    y = 'q0' 
-    
-    pp .Plot2P2H(x,y,histInfo2,"2P2H_hist_max",file_path, max = max_frequency_2P2H, Normalize=1)
+# currently not fixed to reflect PP changes
+fixed = False
+if fixed:
+    for file in root_files:
+        # format correctly
+        file_path = file.split('t2k-nova')[1]
+        file_path = 't2k-nova' + file_path
+        
+        x = 'W'
+        y = 'Q2'
+        histInfo1 = ("name",f"{x} vs {y} plot",60,0,3,124,-0.2,6)
+        histInfo2 = ("name",f"{x} vs {y} plot",60,0,3,60,0,3)
+        
+        pp.Plot1PI(x,y,histInfo1,"1PI_hist_max",file_path, max = max_frequency_1PI, Normalize=1)
+        
+        x = 'q3'
+        y = 'q0' 
+        
+        pp .Plot2P2H(x,y,histInfo2,"2P2H_hist_max",file_path, max = max_frequency_2P2H, Normalize=1)
