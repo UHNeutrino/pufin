43258ea0e1becbb48c8ba02241900d8aa885b24b
Lars Bøe <larsw00@outlook.com>
2025-01-28 13:46:32 -0600

    overhaul of PP and PM for future proofing

--- Plot_macro.py
+++ Plot_macro.py
@@ -1,4 +1,4 @@
-import ParticlePlots
+import ParticlePlots as pp
 import glob
 import os
 
@@ -18,12 +18,14 @@ for file in root_files:
     # format correctly
     file_path = file.split('t2k-nova')[1]
     file_path = 't2k-nova' + file_path
-    # print(file_path)
+  
 
     x = 'W'
     y = 'Q2'
+    AxisInfo = ['q_{3}', '(GeV)','q_{0}', '(GeV)']
     histInfo1 = ("name",f"{x} vs {y} plot",60,0,3,124,-0.2,6)
     histInfo2 = ("name",f"{x} vs {y} plot",60,0,3,60,0,3)
+<<<<<<< HEAD
     
     # Generate normalized plot for 1PI and get the histogram object
     hist_1PI = ParticlePlots.Plot1PI(x, y, histInfo1, "1PI_hist_Normalized", file_path, Normalize=1)
@@ -50,6 +52,24 @@ for file in root_files:
 
     ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist",file_path)
     ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist_Normalized",file_path, Normalize = 1)
+=======
+    hist, path = pp.Plot1PI(x,y,histInfo1,file_path)
+    pp.SavePlot(hist,"1PI_hist", AxisInfo, path)
+    hist, path = pp.Plot1PI(x,y,histInfo1,file_path)
+    pp.SavePlot(hist, "1PI_hist_max", AxisInfo, path, max = .035, Normalize=1)
+    hist, path = pp.Plot1PI(x,y,histInfo1,file_path)
+    pp.SavePlot(hist,"1PI_hist_Normalized", AxisInfo,path, Normalize = 1)
+
+    x = 'q3'
+    y = 'q0' 
+    AxisInfo = ['W', '(GeV)','Q^{2}', '(GeV)^{2}']
+    hist, path = pp.Plot2P2H(x,y,histInfo2,file_path)
+    pp.SavePlot(hist,"2P2H_hist", AxisInfo, path)
+    hist, path = pp.Plot2P2H(x,y,histInfo2,file_path)
+    pp.SavePlot(hist,"2P2H_hist_max", AxisInfo, path, max = .016, Normalize = 1)
+    hist, path = pp.Plot2P2H(x,y,histInfo2,file_path)
+    pp.SavePlot(hist,"2P2H_hist_Normalized", AxisInfo, path, Normalize = 1)
+>>>>>>> 1bdb1a5 (overhaul of PP and PM for future proofing)
     
 print(f"The highest frequency across all normalized 1PI plots is: {max_frequency_1PI}")
 print(f"The highest frequency across all normalized 2P2H plots is: {max_frequency_2P2H}")
