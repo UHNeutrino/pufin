23d3c6433d9737e79465a1cbccf2729cf36a4b56
Karim Ahmed Attia Hassinin <karimhassinin@gmail.com>
2025-03-06 12:31:13 -0600

    merge test

--- Plot_macro.py
+++ Plot_macro.py
@@ -23,14 +23,14 @@ for file in root_files:
     y = 'Q2'
     histInfo1 = ("name",f"{x} vs {y} plot",60,0,3,124,-0.2,6)
     histInfo2 = ("name",f"{x} vs {y} plot",60,0,3,60,0,3)
-    ParticlePlots.Plot1PI(x,y,histInfo1,"1PI_hist",file_path)
-    ParticlePlots.Plot1PI(x,y,histInfo1,"1PI_hist_max",file_path, max = 80000)
-    ParticlePlots.Plot1PI(x,y,histInfo1,"1PI_hist_Normalized",file_path, Normalize = 1)
+    # ParticlePlots.Plot1PI(x,y,histInfo1,"1PI_hist",file_path)
+    ParticlePlots.Plot1PI(x,y,histInfo1,"1PI_hist_max",file_path, max = .06, Normalize = 1)
+    # ParticlePlots.Plot1PI(x,y,histInfo1,"1PI_hist_Normalized",file_path, Normalize = 1)
 
     x = 'q3'
     y = 'q0' 
-
-    ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist",file_path)
-    ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist_max",file_path, max = 80000)
-    ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist_Normalized",file_path, Normalize = 1)
+ 
+    # ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist",file_path)
+    ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist_max",file_path, max = .03, Normalize=1)
+    # ParticlePlots.Plot2P2H(x,y,histInfo2,"2P2H_hist_Normalized",file_path, Normalize = 1)
     
