9a428438d6335f0817a8450ba4ac29bdc75031f5
Lars Bøe <larsw00@outlook.com>
2025-03-13 11:38:47 -0500

    1Pi Minoo changes

--- 1PiObservables.py
+++ 1PiObservables.py
@@ -1,4 +1,5 @@
 import ROOT
+import os
 import PlotQuantiles as pq
 import ParticlePlots as pp 
 import SetupFunctions as sf
@@ -7,6 +8,7 @@ import glob
 
 
 # file_path = '/data/t2k-nova/FlatTrees/Flat_NEUT_0.7GeV_1e6v2.root'
+HOME = os.getenv("HOME", "/home/lboe")
 userFolder = f"/data/t2k-nova/FlatTrees"
 montecarlo = input("Enter the name of your montecarlo: NEUT, NOvA, or ICARUS. If you want to generate plots for all Flat Trees in the directory hit 'enter' ")
 
@@ -14,8 +16,13 @@ montecarlo = input("Enter the name of your montecarlo: NEUT, NOvA, or ICARUS. If
 root_files = glob.glob(userFolder + f'/*{montecarlo}*.root')
 print(f"Root Files: {root_files}")
 
+modeDic = sf.modeDic()
 
 print("Making File Plots: ")
+
+# deleting text files to reset them
+os.system(f"rm {HOME}/t2k-nova/minooValues.txt")
+
 for file_path in root_files:
     file_name = file_path.split('/')[-1]
     generator = file_name.split('_')[1]
@@ -27,7 +34,7 @@ for file_path in root_files:
     AxisInfo1 = ['W', '(GeV)','Q^{2}', '(GeV)^{2}', plottitle]
     df1Pi= pp.CreateDataFrame(file_path, "Mode == 11 || Mode ==  12 || Mode == 13 || Mode == 14 || Mode == 15 || Mode == 16 ")
     hist2p_q = pp.Create2DHistogram(df1Pi,'W','Q2',histInfo)
-    pp.Savehist(hist2p_q,AxisInfo1,"t2k-nova/1PiPlots",f"{flux}1PiWvQ2V2")
+    # pp.Savehist(hist2p_q,AxisInfo1,"t2k-nova/1PiPlots",f"{flux}1PiWvQ2V2")
 
     # Evis vs Etrue
     # plottitle = "E_{#nu true} vs E_{av} for 1Pi events at #nu_{E} =" + flux
@@ -39,7 +46,10 @@ for file_path in root_files:
 
 
     # Minoo sections
-
+    totalE = df1Pi.Count().GetValue()
+    with open(f"{HOME}/t2k-nova/minooValues.txt", "a") as f:
+                f.write(f"For {generator} at Enu = {flux} \n")
+                f.write(f"Total Events: {totalE} \n \n")
     Wevo = {"1st Resonance Region": [1.1,1.4], "2nd Resonance Region": [1.4,1.6], "3rd Resonance Region": [1.6,2.0],"DIS": [2.0,2.4]}
     Q2evo = {"non-preterbative region": [0,1.0], "Transition Region": [1.4,1.6]}
     regionnum = 0
@@ -47,29 +57,54 @@ for file_path in root_files:
     for reigon in Wevo:
         plottitle = reigon + " " + "W vs Q^{2} for 1Pi events at #nu_{E} = " + flux
         AxisInfo1 = ['W', '(GeV)','Q^{2}', '(GeV)^{2}', plottitle]
-        print(Wevo[reigon][0])
-        print(Wevo[reigon][1])
+        # print(Wevo[reigon][0])
+        # print(Wevo[reigon][1])
         
         histInfo = ("name",f"plop1",60,1.1,2.4,105,-0.1,5)
         Wdf = df1Pi.Filter(f"W >= {Wevo[reigon][0]} && W < {Wevo[reigon][1]}")
+        if  Wdf.Count().GetValue() == 0:
+            print("No events in reigon, break")
+            with open(f"{HOME}/t2k-nova/minooValues.txt", "a") as f:
+                f.write("No events in reigon, break \n")
+            break
         Wevo[reigon] = Wdf
         hist2p_q = pp.Create2DHistogram(Wdf,'W','Q2',histInfo)
         regionnum += 1
-        pp.Savehist(hist2p_q,AxisInfo1,"t2k-nova/1PiPlots",f"{flux}1PiWvQ2V2Reigon{regionnum}")
+        # pp.Savehist(hist2p_q,AxisInfo1,"t2k-nova/1PiPlots",f"{flux}1PiWvQ2V2Reigon{regionnum}")
+
+        # Printing event numbers for each reigon (Make diagram in google slides)
         Pimodes = [11,12,13,15,16]
-        colors = [ROOT.kRed ,ROOT.kViolet, ROOT.kYellow, ROOT.kBlue, ROOT.kGreen, ROOT.kOrange]
-        
-        histinfo1D = ("name",f"plop1",40,0.5,2.5)
-        stack, histlist, Legend = pp.PlotStackedEventModes(Wdf, histinfo1D, Pimodes, colors)
-        AxisInfo = ["W (GeV)", "Events", reigon + " Stacked events vs W for #nu_{E} = " + flux]
-        pp.SaveStackedHist(stack, histlist, AxisInfo, Legend,f"/home/lboe/t2k-nova/1PiPlots/{flux}stacked_eventsReigon{regionnum}.png")
+                
+        for mode in Pimodes:
+            modedf = Wdf.Filter(f"Mode == {mode}")
+            totalN = Wdf.Count().GetValue()
+            filteredN = modedf.Count().GetValue()
+
+            
+            percent = filteredN/totalN
+
+            printstring = str(modeDic.get(mode)) + " Events in " + str(reigon) + ": "+ str(filteredN) + " out of " + str(totalN) +  f" ({percent} %)"
+            print()
+            print(printstring)
+            print()
+            #File Writing
+            with open(f"{HOME}/t2k-nova/minooValues.txt", "a") as f:
+                f.write(f"{printstring}\n")
+        # Stacked Event Plotting
+        # colors = [ROOT.kRed ,ROOT.kViolet, ROOT.kYellow, ROOT.kBlue, ROOT.kGreen, ROOT.kOrange]
+        # histinfo1D = ("name",f"plop1",40,0.5,2.5)
+        # stack, histlist, Legend = pp.PlotStackedEventModes(Wdf, histinfo1D, Pimodes, colors)
+        # AxisInfo = ["W (GeV)", "Events", reigon + " Stacked events vs W for #nu_{E} = " + flux]
+        # pp.SaveStackedHist(stack, histlist, AxisInfo, Legend,f"/home/lboe/t2k-nova/1PiPlots/{flux}stacked_eventsReigon{regionnum}.png")
+        with open(f"{HOME}/t2k-nova/minooValues.txt", "a") as f:
+                f.write("\n \n")
         
 
     
 
 
 
-    # E_kinQE vs E true (REC) 
+    # E_kinQE vs E true 
     # plottitle = "E_{#nu true} vs E_{av} for 1Pi events at #nu_{E} =" + flux
     # histInfo2 = ("name",f"plop1",40,0.5,.9,40,0,0.85)
     # AxisInfo2 = ['E_{#nu true}', '(GeV)','E_{av}', '(GeV)', plottitle]
@@ -78,11 +113,11 @@ for file_path in root_files:
 
 
     # enu_QE vs enu_TRUE - this is working, needs formatting to move the stats box to the left edge
-    plottitle = "Enu_QE vs E_{#nu true} for 1Pi events at #nu_{E} =" + flux
-    histInfo2 = ("name",f"plop1",100,0,3.5,100,0,3.5)
-    AxisInfo2 = ['E_{#nu true}', '(GeV)','Enu_QE', '(GeV)', plottitle]
-    hist2p_q = pp.Create2DHistogram(df1Pi,'Enu_true','Enu_QE',histInfo2)
-    pp.Savehist(hist2p_q,AxisInfo2,"t2k-nova/1PiPlots",f"1PiE(QE)vE_{flux}_{generator}")
+    # plottitle = "Enu_QE vs E_{#nu true} for 1Pi events at #nu_{E} =" + flux
+    # histInfo2 = ("name",f"plop1",100,0,3.5,100,0,3.5)
+    # AxisInfo2 = ['E_{#nu true}', '(GeV)','Enu_QE', '(GeV)', plottitle]
+    # hist2p_q = pp.Create2DHistogram(df1Pi,'Enu_true','Enu_QE',histInfo2)
+    # pp.Savehist(hist2p_q,AxisInfo2,"t2k-nova/1PiPlots",f"1PiE(QE)vE_{flux}_{generator}")
 
     # TKI variables (look at talks or something)
     # plottitle = "E_{#nu true} vs E_{av} for 1Pi events at #nu_{E} =" + flux
