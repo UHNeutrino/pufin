6ad84e10133ab40f5ca8c24782e6290695dff9a2
kdobbs3 <kdobbs@uhneutrino.localdomain>
2025-03-17 12:56:08 -0500

    3 versions of Evis, TKI, enu_QE vs Enutrue

--- 1PiObservables.py
+++ 1PiObservables.py
@@ -36,21 +36,34 @@ for file_path in root_files:
     hist2p_q = pp.Create2DHistogram(df1Pi,'W','Q2',histInfo)
     # pp.Savehist(hist2p_q,AxisInfo1,"t2k-nova/1PiPlots",f"{flux}1PiWvQ2V2")
 
-    # Evis vs Etrue - Work in Progress
+    # Evis vs Etrue - add minium thresholds for all of these?
     
-    # Version 1: Evis = EvAlt (q0 - KE neutrons - mass pions) + ELep - working - add minimum threshold?
-    # plottitle = f"{generator}: E_{{vis}} vs E_{{#nu true}} for 1Pi events at #nu_{{E}} =" + flux
+    # Version 1: Evis = EvAlt (q0 - KE neutrons - mass pions) + ELep  
+    # plottitle = f"{generator}: E_{{vis1}} vs E_{{#nu true}} for 1Pi events at #nu_{{E}} =" + flux
     # histInfo2 = ("name",f"plop1",100,0,3.5,100,0,3.5)
     # AxisInfo2 = ['E_{#nu true}', '(GeV)','E_{vis} = q0 - KE_{neutrons} - Mass_{pions} + E_{#mu}', '(GeV)', plottitle]
     # hist2p_q = pp.Create2DHistogram(df1Pi,'Enu_true','Evis_1',histInfo2)
     # pp.Savehist(hist2p_q,AxisInfo2,"t2k-nova/1PiPlots",f"1PiE(V1)vE_{flux}_{generator}")
 
-    # Version 2: Evis = working on this
-    # plottitle = f"{generator}: E_{{vis}} vs E_{{#nu true}} for 1Pi events at #nu_{{E}} =" + flux
+    # Version 2 (from Erecoil in Nuiance github): Evis2 = KE (protons & charged pions) + E (pi0, e+/-, photons, muon) 
+    # plottitle = f"{generator}: E_{{vis2}} vs E_{{#nu true}} for 1Pi events at #nu_{{E}} =" + flux
     # histInfo2 = ("name",f"plop1",100,0,3.5,100,0,3.5)
-    # AxisInfo2 = ['E_{#nu true}', '(GeV)','E_{vis} = KE_{protons; charged pions} + TE_{pi0; electrons; positrons; photons} + E_{#mu}', '(GeV)', plottitle]
+    # AxisInfo2 = ['E_{#nu true}', '(GeV)','           E_{vis} = KE_{pr; pi+/-} + E_{pi0; e-/+; photons} + E_{#mu}', '(GeV)', plottitle]
     # hist2p_q = pp.Create2DHistogram(df1Pi,'Enu_true','Evis_2',histInfo2)
     # pp.Savehist(hist2p_q,AxisInfo2,"t2k-nova/1PiPlots",f"1PiE(V2)vE_{flux}_{generator}")
+    
+    # Version 3 (Eavail from NOvA): Evis = Evis2 + skip bindinos & nucleons 
+    # + total energy minus proton mass of (Primarily) strange baryons
+    # since decays will mostly contain protons 
+    # + total energy plus proton mass of (primarily) anti-protons 
+    # since anhillation is mostly the interaction mode
+    # + if no neutrons or leptons (mostly kaons) just add all the energy
+    
+    plottitle = f"{generator}: E_{{vis3}} vs E_{{#nu true}} for 1Pi events at #nu_{{E}} =" + flux
+    histInfo2 = ("name",f"plop1",100,0,3.5,100,0,3.5)
+    AxisInfo2 = ['E_{#nu true}', '(GeV)','E_{vis 3}', '(GeV)', plottitle]
+    hist2p_q = pp.Create2DHistogram(df1Pi,'Enu_true','Evis_3',histInfo2)
+    pp.Savehist(hist2p_q,AxisInfo2,"t2k-nova/1PiPlots",f"1PiE(V3)vE_{flux}_{generator}")
 
 
 
@@ -109,11 +122,7 @@ for file_path in root_files:
                 f.write("\n \n")
         
 
-    
-
-
-
-    # E_kinQE vs E true 
+    # E_kinQE vs E true (REC) - haven't checked
     # plottitle = "E_{#nu true} vs E_{av} for 1Pi events at #nu_{E} =" + flux
     # histInfo2 = ("name",f"plop1",40,0.5,.9,40,0,0.85)
     # AxisInfo2 = ['E_{#nu true}', '(GeV)','E_{av}', '(GeV)', plottitle]
@@ -122,7 +131,7 @@ for file_path in root_files:
 
 
     # enu_QE vs enu_TRUE - this is working, needs formatting to move the stats box to the left edge
-    # plottitle = "Enu_QE vs E_{#nu true} for 1Pi events at #nu_{E} =" + flux
+    # plottitle = f"{generator}: Enu_QE vs E_{{#nu true}} for 1Pi events at #nu_{{E}} =" + flux
     # histInfo2 = ("name",f"plop1",100,0,3.5,100,0,3.5)
     # AxisInfo2 = ['E_{#nu true}', '(GeV)','Enu_QE', '(GeV)', plottitle]
     # hist2p_q = pp.Create2DHistogram(df1Pi,'Enu_true','Enu_QE',histInfo2)
@@ -144,7 +153,7 @@ for file_path in root_files:
     # hist2p_q = pp.Create2DHistogram(df1Pi,'PLep','TKI_ON',histInfo2)
     # pp.Savehist(hist2p_q,AxisInfo2,"t2k-nova/1PiPlots",f"1PiTKI_ONvp_{flux}_{generator}")
 
-    # Proton kinematics
+    # Proton kinematics - haven't checked
     # plottitle = "E_{#nu true} vs E_{av} for 1Pi events at #nu_{E} =" + flux
     # histInfo2 = ("name",f"plop1",100,0,1,100,0,1)
     # AxisInfo2 = ['E_{#nu true}', '(GeV)','E_{av}', '(GeV)', plottitle]
