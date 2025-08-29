ee0082e506f17ffcdc9647b8a329488e7c5b593c
Lars Bøe <larsw00@outlook.com>
2025-08-29 14:50:51 -0500

    first draft of systematics tweakinggit add nusystrun/* sysMain.json5 systPlotter.py

---
+++ nusystrun/nusysSave.cpp
@@ -0,0 +1,108 @@
+// R__ADD_LIBRARY_PATH($LD_LIBRARY_PATH)
+R__ADD_LIBRARY_PATH("/project/software/GENIE_v2_06_00/Generator/install/lib")
+R__LOAD_LIBRARY(libGRwClc.so)
+#include "nusystematics/utility/response_helper.hh"
+#include "Framework/Ntuple/NtpMCEventRecord.h"
+#include "Framework/EventGen/EventRecord.h"
+
+
+#include "/home/lboe/nusystrun/include/user_int/Get_User_stuff.hh"
+#include "/home/lboe/nusystrun/include/draw_struct/CreateXML.hh"
+
+#include <vector>
+#include <iostream>
+
+#include <TRandom3.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TBranch.h>
+#include <TStopwatch.h>
+#include <TH1D.h>
+#include <ROOT/RDataFrame.hxx>
+
+void nusysSave(){
+    std::string fcl_file = "/home/mazen_malak/Thesis_results/all.fcl";
+    std::string Genie_path_name = "/data/t2k-nova/LarsGen/GenieGen/GENIEN24FORREAL_ff";  
+    std::string Genie_file_str  = Genie_path_name + ".root";  
+    std::string OutPath_str     = Genie_path_name + "_sysweights.root";  
+    const char* Genie_file = Genie_file_str.c_str(); // Genie file path
+    const char* OutPath    = OutPath_str.c_str();
+
+    std::string userdata = "/data/t2k-nova/LarsGen/GenieGen/N24Config.txt"; //Config file path??
+    std::cout << "hi" << std::endl;
+
+    // ############################################################
+    TFile* genie_file = TFile::Open(Genie_file, "READ"); //String for Genie event record preflattened gen
+
+    if (!genie_file || genie_file->IsZombie()) {
+        std::cerr << " Error: could not open file " << Genie_file << std::endl;
+        delete genie_file; // cleanup
+        return;
+    }
+
+    std::cout << "Successfully opened " << Genie_file << std::endl;
+    std::cout << Get_GENIE_file_and_tune(userdata)[0] << std::endl;
+    TTree* mytree = (TTree*)genie_file->Get("gtree");
+
+    // clone tree to put weights in
+    TFile* outfile = new TFile(OutPath, "RECREATE");
+    TTree* outtree = new TTree("weighttree","weighttree"); // 0 = copy only structure (branches, leaves)
+    std::cout << "Cloned Tree" << Genie_file << std::endl;
+
+    genie::NtpMCEventRecord* myEventRecord = new genie::NtpMCEventRecord();
+    mytree->SetBranchAddress("gmcrec",&myEventRecord);
+    // // genie_file->Close();
+    nusyst::response_helper* resp = new nusyst::response_helper(fcl_file.c_str());
+    std::cout << "hi2" << std::endl;
+    // ###########################################################################
+    // ^This block pulls the GENIE Event Record followed by setting up nusystematics reposnsehelper where you pass it a fcl file
+    std::fstream syst_file;
+    std::vector<string> syst_name = Get_Syst_name(userdata);
+    std::vector<systtools::paramId_t > syst_id;
+    std::cout << syst_name.size() << std::endl;
+
+
+    for(int i = 0; i<syst_name.size(); i++ ){
+        std::cout << syst_name[i] << std::endl;
+        syst_id.push_back(resp->GetHeaderId(syst_name[i]));
+    }
+    
+    // defining branches for weights
+    std::vector<double> syst_weights(syst_id.size(), 1.0); //create vector to hold all the weights
+    std::vector<double*> syst_ptrs(syst_id.size(), nullptr);
+    for (size_t i = 0; i < syst_name.size(); i++) {
+        syst_ptrs[i] = &syst_weights[i];
+        outtree->Branch(syst_name[i].c_str(), syst_ptrs[i]);
+    }
+
+    int num_of_events = Get_Num_Events(userdata);
+    std::cout <<  "Events:" << std::endl;
+    std::cout << num_of_events << std::endl;
+
+
+    // The stuff before the temp spline is important
+    for(int event_num =0; event_num<num_of_events; event_num++){
+        if(event_num%1000==0){
+            std::cout << event_num << std::endl;
+        }
+        // std::cout << "Get Events" << std::endl;
+        mytree->GetEntry(event_num);
+        // std::cout << "Grab Nodes" << std::endl;
+        const genie::EventRecord & event = *(myEventRecord->event);
+        systtools::event_unit_response_t Events_stand = resp->GetEventResponses(event);
+        for(int syst=0;syst<syst_name.size();syst++){
+            // std::cout << "Get Events" << std::endl;
+            // Evaluate spline at chosen dial (example: 1.5) and store in syst_weights
+            syst_weights[syst] = resp->GetSpline(syst_id[syst], Events_stand).Eval(1.5);
+        }
+        outtree->Fill();
+    }
+    outfile->cd();
+    outtree->Write();
+    outfile->Close();
+    genie_file->Close();
+
+    // // ^
+
+
+}
---
+++ systPlotter.py
@@ -0,0 +1,79 @@
+import json5
+import ROOT
+import os
+import datetime
+import ParticlePlots as pp 
+import SetupFunctions as sf
+import PlotQuantiles as pq
+import glob
+import array
+
+
+HOME = os.getenv("HOME", "/home/lboe")
+sf.setupRoot
+userFolder = f"/data/t2k-nova/FlatTrees"
+f = open(f'{HOME}/t2k-nova/sysMain.json5')
+data = json5.load(f)
+
+plots = data.get("plots")
+
+if (plots["Bool"]):
+    root_files = glob.glob(userFolder + f'/*{plots["Gen"]}*{plots["Flux"]}*.root')
+    if root_files == []:
+        print("NO such root files")
+
+    for file_path in root_files:
+        file_name = file_path.split('/')[-1]
+        generator = file_name.split('_')[1]
+        flux = file_name.split('_')[2]
+        BinL = plots["Bins"]
+        AxisInfo = []
+
+        if (plots["sysRW"][0]) :
+            f2 = ROOT.TFile(file_path)
+            ft2 = f2.Get("FlatTree_VARS")
+            ft2.AddFriend(plots["sysRW"][2],plots["sysRW"][1])
+            print( type(ft2))
+            df = ROOT.RDataFrame(ft2)
+            df = df.Filter(plots["Cut"])
+        else:
+            df = pp.CreateDataFrame(file_path, plots["Cut"])
+        if(plots["EvisB"]):
+            df = pp.DefineEvis(df)  
+        if (plots["KinematicsB"]):
+            df = pp.DefineKinematics(df)
+        if (plots["TkiB"]):
+            df = pp.DefineTKI(df)
+        for word in plots["AxisInfo"].split(','):
+            AxisInfo.append(word)
+        if(plots["reWeight"][0]):
+            df = pp.defineWeightsSpline(df,plots["reWeight"][1],plots["reWeight"][2])
+        if plots["Type"] == "1D":
+            histInfo = (AxisInfo[-1],AxisInfo[-1],BinL[0],BinL[1],BinL[2])
+            print(histInfo)
+            if(plots["reWeight"][0]):
+                hist = df.Histo1D(histInfo,plots["Var1"],"weights")
+            elif(plots["sysRW"][0]):
+                hist = df.Histo1D(histInfo,plots["Var1"],"FrAbs_pi")
+            else:
+                hist = df.Histo1D(histInfo,plots["Var1"])
+        if plots["Type"] == "2D":
+            histInfo = (AxisInfo[-1],AxisInfo[-1],BinL[0],BinL[1],BinL[2],BinL[3],BinL[4],BinL[5])
+            if(plots["reWeight"][0]):
+                hist = df.Histo2D(histInfo,plots["Var1"],plots["Var2"],"weights")
+            elif(plots["sysRW"][0]):
+                hist = df.Histo2D(histInfo,plots["Var1"],plots["Var2"],plots["sysRW"][3])
+            else:
+                hist = df.Histo2D(histInfo,plots["Var1"],plots["Var2"])
+        if plots["Ext"] == "root":
+            rootTitle = plots["Cut"]
+            rootTitle = rootTitle.replace(" ","")
+            out_file = ROOT.TFile(f"/data/t2k-nova/Histograms/{rootTitle}.root", "UPDATE")
+            hist.Write()  # Write the histogram to the file
+            out_file.Close()  # Close to finalize writing
+        else:
+            nx = datetime.datetime.now()
+            x = str(nx)
+            fileN = generator+flux+plots["Name"]+x
+            fileN = fileN.replace(" ", "-")
+            pp.Savehist(hist,AxisInfo,plots["Save"],fileN,plots["Ext"],max = plots["max"], Normalize=plots["Norm"], logz = plots["logz"])
