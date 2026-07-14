d0d47730404c20a4c9fb45f7de0314d33491f3fa
kdobbs3 <kdobbs@uhneutrino.localdomain>
2026-07-14 06:25:31 -0500

    adding Genie functionality to GenSubmit.py

--- GenSubmit.py
+++ GenSubmit.py
@@ -51,6 +51,71 @@ def NeutRunScript(Container, Tune, Events, TotalNodes, NChunks, Target=None, Mod
     
     for p in processes:
         p.wait()
+        
+def GenieRunScript(Container, Tune, NChunks, TotalNodes, Target=None, Mode=None, Flavor=None, CPUPercent=None):
+    OutPath = "/data/t2k-nova/PUfINOutputs/_MultiProcess"
+    os.makedirs(OutPath, exist_ok=True)
+
+    OldOutPath = os.environ.get("PUFIN_OUT")
+    os.environ["PUFIN_OUT"] = OutPath
+
+    Generator = "GENIE"
+
+    FilePath, Targets = GenMain.DirectorySetup(Generator, SingleTarget=Target, Mode=Mode)
+    GenMain.FlatFluxMaker()
+
+    FileNames = GenMain.CheckGenieFiles(
+        Tune=Tune,
+        Targets=Targets,
+        Events=NChunks,   # For multiprocessing, this means NuMu NChunks
+        Modes=Mode,
+        Flavors=Flavor,
+    )
+
+    if OldOutPath == None:
+        del os.environ["PUFIN_OUT"]
+    else:
+        os.environ["PUFIN_OUT"] = OldOutPath
+
+    if CPUPercent > 1 and CPUPercent <= 100:
+        CPUPercent /= 100
+    elif CPUPercent > 100 or CPUPercent < 0:
+        raise ValueError("CorePercent must be 0<x leq 1 or 1<x<100")
+
+    NCores = max(1, int(cpu_count()*CPUPercent))
+
+    print(f"Not Sorted {len(FileNames)}")
+    FileNames = sorted(FileNames, key=lambda x: "_NuMu_" not in x)
+    print(f"Total Files: {len(FileNames)}")
+
+    processes = []
+
+    for Node in range(0, TotalNodes):
+        NodeFiles = FileNames[Node::TotalNodes]
+
+        if len(NodeFiles) == 0:
+            print(f"Files on Node {Node}: 0")
+            continue
+
+        SlurmTime = GenieTimeEstimator(NodeFiles)
+
+        print(f"Files on Node {Node}: {len(NodeFiles)}")
+
+        cmd = f"""
+        sbatch \\
+        --nodes=1 \\
+        --ntasks-per-node=1  \\
+        --cpus-per-task={NCores} \\
+        --time={SlurmTime} \\
+        --wrap "apptainer exec --writable-tmpfs --bind {OutPath}:/mnt {Container} bash -c 'source /opt/SetupAll.sh && export PUFIN_OUT={OutPath} && python GenMain.py GenieMult --Files "{NodeFiles}" --CPUPercent {CPUPercent} '"
+        """
+
+        print(cmd)
+        # p = subprocess.Popen(cmd, shell=True)
+        # processes.append(p)
+
+    for p in processes:
+        p.wait()
 
 def TimeEstimator(Files):
     # loop through all files and come up with a decent time estimation using linear regressions from trends found in initial testing
@@ -89,9 +154,21 @@ if __name__ =="__main__":
     GenParser.add_argument("--target", default=None)
     GenParser.add_argument("--mode", default=None)
     GenParser.add_argument("--flavor", default=None)
+    
+    GenieParser = subparsers.add_parser("GenGenie")
+    GenieParser.add_argument("--container", required=True, type=str)
+    GenieParser.add_argument("--tune", default=None)
+    GenieParser.add_argument("--total_nodes", required=True, type=int)
+    GenieParser.add_argument("--cpu_percent", required=True, type=float)
+    GenieParser.add_argument("--nchunks", required=True, type=int)
+    GenieParser.add_argument("--target", default=None)
+    GenieParser.add_argument("--mode", default=None)
+    GenieParser.add_argument("--flavor", default=None)
 
     args = parser.parse_args()
-    NeutRunScript(
+    
+    if args.command == "GenNeut":
+        NeutRunScript(
                 Container=args.container,
                 Events=args.events,
                 Tune=args.tune,
@@ -102,6 +179,30 @@ if __name__ =="__main__":
                 CPUPercent= args.cpu_percent,
                 NChunks= args.nchunks,
             )
+    
+    elif args.command == "GenGenie":
+        GenieRunScript(
+        Container=args.container,
+        Tune=args.tune,
+        NChunks=args.nchunks,
+        TotalNodes=args.total_nodes,
+        Target=args.target,
+        Mode=args.mode,
+        Flavor=args.flavor,
+        CPUPercent=args.cpu_percent,
+    )
+
+# source /data/t2k-nova/MainSetup.sh
+# export PUFIN_OUT=/data/t2k-nova/PUfINOutputs/_MultiProcess
+# python GenSubmit.py GenGenie \
+#   --container /path/to/container.sif \
+#   --tune N24 \
+#   --total_nodes 2 \
+#   --cpu_percent 75 \
+#   --nchunks 10 \
+#   --target Carbon \
+#   --mode CC \
+#   --flavor NuMu
 
 
 
