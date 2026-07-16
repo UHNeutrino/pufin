89e19d919aa55aee6e72be1129132fa84fb91c7f
Lars Bøe <larsw00@outlook.com>
2026-07-16 14:00:15 -0500

    increased estimated time for a baseline

--- GenSubmit.py
+++ GenSubmit.py
@@ -152,7 +152,7 @@ def NeutTimeEstimator(Files, NCores):
     # add time for flattening ~3 min each:
     TotalSeconds += len(Files)*180
     
-    TotalSeconds = TotalSeconds/(NCores/2)
+    TotalSeconds = TotalSeconds
     if TotalSeconds>= 86400:
         raise ValueError("Allocation exceeding 24hrs, use more cores or less chunks")
     t = time.gmtime(TotalSeconds)
